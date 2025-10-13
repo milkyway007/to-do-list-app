39d487198702ac87d1909a9905cc05aad7937448
Liidia Laada <liidia.laada@gmail.com>
2025-10-13 18:59:19 +0300

    Add onClick event to vertical menu item.

--- src/components/verticalMenu/VerticalMenuList.tsx
+++ src/components/verticalMenu/VerticalMenuList.tsx
@@ -9,13 +9,25 @@ import VerticalMenuListLabel from './VerticalMenuListLabel';
  * @returns Returns a VerticalMenuList component.
  */
 function VerticalMenuList() {
+	/**
+	 * OnClick event for vertical menu list item.
+	 * @param itemName vertical menu list item header.
+	 */
+	function handleClick(itemName: string): void {
+		console.log(itemName);
+	}
+
 	return (
 		<>
 			<VerticalMenuListLabel />
 			<ul className={'menu-list'}>
 				{mainMenuListItems.map((item: MenuListItem) => {
 					return (
-						<li key={item.itemName}>
+						<li
+							key={item.itemName}
+							onClick={() => {
+								handleClick(item.itemName);
+							}}>
 							<VerticalMenuListItem {...item} />
 						</li>
 					);
