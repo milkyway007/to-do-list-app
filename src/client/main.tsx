dc6fc8c2bccea0978952c8c163fb1f102a74c27d
Liidia Laada <liidia.laada@gmail.com>
2026-08-03 10:00:47 +0300

    Set up React Query Devtools

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import './app/layout/styles.css';
 import { App } from './app/layout/App.tsx';
@@ -16,6 +17,7 @@ if (!root) throw new Error('Root element not found');
 createRoot(root).render(
 	<StrictMode>
 		<QueryClientProvider client={queryClient}>
+			<ReactQueryDevtools initialIsOpen={false} />
 			<App />
 		</QueryClientProvider>
 	</StrictMode>,
